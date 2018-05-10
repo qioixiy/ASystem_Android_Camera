a79171bbfc449222390e3e4503a7f0129c97649b
qioixiy <qioixiy@gmail.com>
2018-05-11 01:51:28 +0800

    gps的话是检测到移动50米以后，就开始录制

--- app/src/main/java/com/xyz/DrivingRecorder/DeviceSensorService.java
+++ app/src/main/java/com/xyz/DrivingRecorder/DeviceSensorService.java
@@ -3,17 +3,26 @@ package com.xyz.DrivingRecorder; /**
  *
  */
 
+import android.Manifest;
 import android.app.Service;
 import android.content.Context;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.hardware.Sensor;
 import android.hardware.SensorEvent;
 import android.hardware.SensorEventListener;
 import android.hardware.SensorManager;
+import android.location.Criteria;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
 import android.os.Binder;
+import android.os.Build;
 import android.os.Bundle;
 import android.os.IBinder;
 import android.os.PowerManager.WakeLock;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.util.Log;
 
 public class DeviceSensorService extends Service {
@@ -22,6 +31,10 @@ public class DeviceSensorService extends Service {
     private SensorManager mSensorManager;
     private WakeLock mWakeLock;
     private IHandler mHandler;
+
+    private LocationManager locationManager;
+    private String locationProvider;
+    private Location location;
 
     public interface IHandler {
         void handle(String data);
@@ -105,7 +118,119 @@ public class DeviceSensorService extends Service {
                 Log.i(TAG, "after SystemStatus:" + StaticValue.getSystemStatus());
             }
         });
+
+        initLocation();
     }
+
+    private void initLocation() {
+
+        // 要申请的权限 数组 可以同时申请多个权限
+        String[] permissions = {Manifest.permission.ACCESS_COARSE_LOCATION};
+
+        if (Build.VERSION.SDK_INT >= 23) {
+            //如果超过6.0才需要动态权限，否则不需要动态权限
+            //如果同时申请多个权限，可以for循环遍历
+            int check = ContextCompat.checkSelfPermission(this,permissions[0]);
+            // 权限是否已经 授权 GRANTED---授权  DINIED---拒绝
+            if (check == PackageManager.PERMISSION_GRANTED) {
+                //写入你需要权限才能使用的方法
+                //run();
+            } else {
+                //手动去请求用户打开权限(可以在数组中添加多个权限) 1 为请求码 一般设置为final静态变量
+                //requestPermissions(new String[]{Manifest.permission.ACCESS_COARSE_LOCATION}, 1);
+            }
+        } else {
+            //写入你需要权限才能使用的方法
+            //run();
+        }
+
+        locationManager = (LocationManager) getSystemService(getApplicationContext().LOCATION_SERVICE);
+        Criteria criteria = new Criteria();
+        criteria.setAccuracy(Criteria.ACCURACY_COARSE);//低精度，如果设置为高精度，依然获取不了location。
+        criteria.setAltitudeRequired(false);//不要求海拔
+        criteria.setBearingRequired(false);//不要求方位
+        criteria.setCostAllowed(true);//允许有花费
+        criteria.setPowerRequirement(Criteria.POWER_LOW);//低功耗
+
+        //从可用的位置提供器中，匹配以上标准的最佳提供器
+        locationProvider = locationManager.getBestProvider(criteria, true);
+        if (ActivityCompat.checkSelfPermission(getApplicationContext(),
+                Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(getApplicationContext(),
+                Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            Log.d(TAG, "onCreate: 没有权限 ");
+            return;
+        }
+        Location location = locationManager.getLastKnownLocation(locationProvider);
+        Log.d(TAG, "onCreate: " + (location == null) + "..");
+        if (location != null) {
+            Log.d(TAG, "onCreate: location");
+            //不为空,显示地理位置经纬度
+            //showLocation(location);
+        }
+        //监视地理位置变化
+        locationManager.requestLocationUpdates(locationProvider, 0, 0, locationListener);
+    }
+
+    LocationListener locationListener = new LocationListener() {
+
+        @Override
+        public void onStatusChanged(String provider, int status, Bundle arg2) {
+
+        }
+
+        @Override
+        public void onProviderEnabled(String provider) {
+            Log.d(TAG, "onProviderEnabled: " + provider + ".." + Thread.currentThread().getName());
+        }
+
+        @Override
+        public void onProviderDisabled(String provider) {
+            Log.d(TAG, "onProviderDisabled: " + provider + ".." + Thread.currentThread().getName());
+        }
+
+        @Override
+        public void onLocationChanged(Location location) {
+            Log.d(TAG, "onLocationChanged: " + ".." + Thread.currentThread().getName());
+            //如果位置发生变化,重新显示
+
+            double tCurLatitude = location.getLatitude();
+            double tCurLongitude = location.getLongitude();
+
+            double dist = getDistance(tCurLatitude, tCurLongitude, mPrevLatitude, mPrevLongitude); // unit:KM
+
+            if (mIsFirst) {
+                mIsFirst = false;
+
+                mPrevLatitude = tCurLatitude;
+                mPrevLongitude = tCurLongitude;
+
+                return;
+            }
+
+            if (dist > 50/1000.) {
+                mPrevLatitude = tCurLatitude;
+                mPrevLongitude = tCurLongitude;
+
+                if (mHandler != null) {
+                    mHandler.handle("active_trigger");
+                }
+            }
+        }
+
+        public double getDistance(double lat1, double lon1, double lat2, double lon2) {
+
+            float[] results=new float[1];
+
+            Location.distanceBetween(lat1, lon1, lat2, lon2, results);
+
+            return results[0];
+
+        }
+
+        private boolean mIsFirst = true;
+        private double mPrevLatitude = 0.0;
+        private double mPrevLongitude = 0.0;
+    };
 
     @Override
     public IBinder onBind(Intent intent) {
